974ff712b1efec2d3ddc449dee754511235c78f5
Max Jöhnk <maxjoehnk@gmail.com>
2017-09-03 18:28:20 +0200

    [championgg] add support for multiple roles

--- src/championgg.js
+++ src/championgg.js
@@ -1,7 +1,15 @@
 const fetch = require('node-fetch');
 const d = require('debug')('league-api:champion.gg');
 
-const champion = async(key, id) => {
+const roleFilter = {
+    top: 'TOP',
+    jungle: 'JUNGLE',
+    mid: 'MIDDLE',
+    adc: 'DUO_CARRY',
+    support: 'DUO_SUPPORT'
+};
+
+const allRoles = async(key, id) => {
     d(`Fetching Stats for ${id}`);
     const url = `http://api.champion.gg/v2/champions/${id}?champData=hashes&api_key=${key}`;
     const res = await fetch(url);
@@ -10,10 +18,45 @@ const champion = async(key, id) => {
         d('Error Body', body);
         throw new Error('Invalid Response');
     }
-    const [result] = body;
+    if (body.length === 0) {
+        throw new Error('Invalid Champion');
+    }
+    return body;
+};
+
+const mostPlayed = async(key, id) => {
+    const roles = await allRoles(key, id);
+    return roles.sort((a, b) => a.playRate - b.playRate)[0];
+};
+
+const byRole = async(key, id, role) => {
+    const roles = await allRoles(key, id);
+    const result = roles.find(champion => champion.role === roleFilter[role]);
+    if (!result) {
+        throw new Error('Invalid Role');
+    }
     return result;
 };
 
+const getLabelForRole = role => {
+    switch (role) {
+        case 'TOP':
+            return 'Top';
+        case 'JUNGLE':
+            return 'Jungle';
+        case 'MIDDLE':
+            return 'Mid';
+        case 'DUO_CARRY':
+            return 'ADC';
+        case 'DUO_SUPPORT':
+            return 'Support';
+    }
+}
+
 module.exports = {
-    champion
+    allRoles,
+    byRole,
+    mostPlayed,
+    getLabelForRole,
+    roleFilter
 };
